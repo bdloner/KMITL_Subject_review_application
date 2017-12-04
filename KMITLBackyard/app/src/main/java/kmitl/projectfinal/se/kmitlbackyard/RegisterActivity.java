7c93edf2a1ea1abc4de82d841bdfce1a5f7efeb5
bosstanayot <boss10520@hotmail.com>
2017-12-05 02:26:31 +0700

    register

--- KMITLBackyard/app/src/main/java/kmitl/projectfinal/se/kmitlbackyard/RegisterActivity.java
+++ KMITLBackyard/app/src/main/java/kmitl/projectfinal/se/kmitlbackyard/RegisterActivity.java
@@ -2,28 +2,48 @@ package kmitl.projectfinal.se.kmitlbackyard;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends Activity {
-
+    boolean check = false;
     private EditText regEmail, regPassword, regConfirmPassword, regNickname;
     private Button btnRegister, btnBack;
     private String sRegEmail, sRegPassword, sRegConfirmPassword, sRegNickname;
+    private Firebase mFirebase;
+    private List<String> mChats = new ArrayList<String>();
+    private ArrayAdapter<String> mAdapter;
+    String checkEmail;
+    private FirebaseAuth firebaseAuth;
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[kmitl.-]{5}+\\.[ac]{2}+\\.[th]{2}$", Pattern.CASE_INSENSITIVE);
     int cnt_id;
@@ -32,7 +52,8 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        firebaseAuth = FirebaseAuth.getInstance();
+        mFirebase = new Firebase("https://kmitlbackyard.firebaseio.com/");
         regEmail = findViewById(R.id.reg_email);
         regPassword = findViewById(R.id.reg_password);
         regConfirmPassword = findViewById(R.id.reg_confirm_password);
@@ -40,7 +61,6 @@ public class RegisterActivity extends Activity {
 
         btnRegister = findViewById(R.id.btn_register);
         btnBack = findViewById(R.id.btn_back);
-        
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +70,7 @@ public class RegisterActivity extends Activity {
                 sRegConfirmPassword  = regConfirmPassword.getText().toString();
                 sRegNickname = regNickname.getText().toString();
                 Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(sRegEmail);
+                //sameEmail();
                 if(sRegEmail.equals("") || sRegPassword.equals("") || sRegConfirmPassword.equals("") || sRegNickname.equals("")){
                     Toast.makeText(getApplicationContext(), "กรุณากรอกข้อมูลให้ครบ", Toast.LENGTH_SHORT).show();
                     regPassword.setText("");
@@ -66,7 +87,7 @@ public class RegisterActivity extends Activity {
                     regConfirmPassword.setText("");
                 }
                 else {
-                    addData();
+                    registerUser();
                 }
             }
         });
@@ -80,5 +101,26 @@ public class RegisterActivity extends Activity {
     }
 
     private void addData() {
+        Map<String, Object> his = new HashMap();
+        his.put("email", String.valueOf(sRegEmail));
+        his.put("password", String.valueOf(sRegPassword));
+        his.put("nickname", String.valueOf(sRegNickname));
+        his.put("image", "image");
+        mFirebase.child("user").push().setValue(his);
+    }
+
+    public void registerUser(){
+        firebaseAuth.createUserWithEmailAndPassword(sRegEmail.trim(),sRegPassword.trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            addData();
+                            Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Could not register", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
