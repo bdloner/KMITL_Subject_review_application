a9be450edac87a24bb6cbe71c497e6bd3a1ca048
bosstanayot <boss10520@hotmail.com>
2017-12-07 17:56:40 +0700

    Comment is Real

--- KMITLBackyard/app/src/main/java/kmitl/projectfinal/se/kmitlbackyard/CommentActivity.java
+++ KMITLBackyard/app/src/main/java/kmitl/projectfinal/se/kmitlbackyard/CommentActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -13,13 +15,20 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CommentActivity extends Activity{
 
@@ -29,6 +38,9 @@ public class CommentActivity extends Activity{
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user;
     private DatabaseReference mDatabase;
+    private List<CommentModel> listComments;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +51,8 @@ public class CommentActivity extends Activity{
         avaSend = findViewById(R.id.ava_send);
         addComment = findViewById(R.id.add_comment);
         timeStamp = getCurrentTime();
+        recyclerView = findViewById(R.id.comment_listview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mDatabase = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
@@ -46,6 +60,8 @@ public class CommentActivity extends Activity{
         if(bundle != null){
             post_id = bundle.getString("post_id");
         }
+        listComments = new ArrayList<>();
+        queryData();
         addComment.addTextChangedListener(new TextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {
@@ -97,5 +113,45 @@ public class CommentActivity extends Activity{
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         return dateFormat.format(date);
+    }
+
+    private void queryData() {
+        Query query = mDatabase.child("comment");
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map<String, Object> newComment = (Map<String, Object>) dataSnapshot.getValue();
+                CommentModel commentItem = new CommentModel(
+                        newComment.get("content").toString(), newComment.get("post_id").toString(),
+                        newComment.get("timeStamp").toString(), newComment.get("uid").toString()
+                       );
+                    if(post_id.equals(newComment.get("post_id").toString())){
+                        listComments.add(commentItem);
+                        adapter = new CommentAdapter(listComments, getApplication());
+                        recyclerView.setAdapter(adapter);
+                    }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
