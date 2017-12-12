4031b2ab9e813faecc92c594c7b95e973681d16d
surasee2012 <30916904+surasee2012@users.noreply.github.com>
2017-12-12 23:31:31 +0700

    Update android test
    - Update Main activity test.

--- KMITLBackyard/app/src/androidTest/java/kmitl/projectfinal/se/kmitlbackyard/MainActivityTest.java
+++ KMITLBackyard/app/src/androidTest/java/kmitl/projectfinal/se/kmitlbackyard/MainActivityTest.java
@@ -1,9 +1,9 @@
 package kmitl.projectfinal.se.kmitlbackyard;
 
 import android.os.SystemClock;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -56,6 +56,7 @@ public class MainActivityTest {
     @Test
     public void testSpinner() {
         onView(withId(R.id.spinner_type)).perform(click());
+        SystemClock.sleep(500);
         onData(allOf(is(instanceOf(String.class)), is("ทุกหมวดวิชา"))).perform(click());
         SystemClock.sleep(500);
         onView(withId(R.id.spinner_type)).check(matches(withSpinnerText(containsString("ทุกหมวดวิชา"))));
@@ -86,14 +87,50 @@ public class MainActivityTest {
     @Test
     public void testComment() {
         onView(withId(R.id.navigation_news)).perform(click());
-        SystemClock.sleep(2500);
+        SystemClock.sleep(2000);
 
-        onView(withText("กันเอง นักเลงพอ")).check(matches(isDisplayed()));
+        onView(withId(R.id.list_post)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.post_comment)));
+        onView(withId(R.id.add_comment)).perform(click());
+
+        onView(withId(R.id.add_comment)).perform(replaceText("hello world"));
+        onView(withId(R.id.ava_send)).perform(click());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void testLike() {
+        onView(withId(R.id.navigation_news)).perform(click());
+        SystemClock.sleep(2000);
 
+        onView(withId(R.id.list_post)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.post_love)));
+    }
+
+    @Test
+    public void testRename() {
+        onView(withId(R.id.navigation_me)).perform(click());
+        SystemClock.sleep(500);
+
+        onView(withId(R.id.profile_nickname)).perform(replaceText("hello world"));
+        onView(withId(R.id.save_btn)).perform(click());
+        SystemClock.sleep(1500);
+    }
+
+    @Test
+    public void testViewMyPost() {
+        onView(withId(R.id.navigation_me)).perform(click());
+        SystemClock.sleep(500);
+
+        onView(withId(R.id.history_img)).perform(click());
+    }
+
+    @Test
+    public void testViewProfile() {
+        onView(withId(R.id.navigation_news)).perform(click());
+        SystemClock.sleep(2500);
+
+        onView(withId(R.id.list_post)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.image_icon)));
     }
 
 }
